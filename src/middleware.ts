6ddb43d1c5be188bd1371aff4e9e61d8f2e9f6c9
abdulvohid31 <abdulvohidcode@gmail.com>
2025-10-21 11:52:53 +0500

    i18n moved

--- src/middleware.ts
+++ src/middleware.ts
@@ -32,7 +32,7 @@
 
 
 import createMiddleware from "next-intl/middleware";
-import { routing } from "./i18n/routing";
+import { routing } from "./assets/i18n/routing";
 
 export default createMiddleware(routing);
 
