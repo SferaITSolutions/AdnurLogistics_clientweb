d560498b4a49b203258b9a5b64ceb80983270de7
abdulvohid31 <abdulvohidcode@gmail.com>
2025-10-22 11:00:35 +0500

    HOOKS

--- src/entities/hooks/order/hooks.ts
+++ src/entities/hooks/order/hooks.ts
@@ -1,15 +1,15 @@
 import dashboardService from "@/services/client-dashboard/dashboard.service";
 import { useQuery } from "@tanstack/react-query";
 
-export const useOrder = () => {
+export const useOrder = (data: any) => {
   return useQuery({
-    queryKey: ['order'],
-    queryFn: (data) => dashboardService.getOrdersData(data),
+    queryKey: ["order"],
+    queryFn: () => dashboardService.getOrdersData(data),
   });
 };
 export const useOrderById = (id: string) => {
   return useQuery({
-    queryKey: ['order', id],
+    queryKey: ["order", id],
     queryFn: () => dashboardService.getOrderById(id),
     enabled: !!id,
   });
