4a71325abd887040e0f85e38a0f042bf22a05ba1
abdulvohid31 <abdulvohidcode@gmail.com>
2025-10-25 16:25:44 +0500

    qw

--- .next/server/pages/_app.js
+++ .next/server/pages/_app.js
@@ -1,4 +1,4 @@
 var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_app.js")
-R.c("server/chunks/ssr/[root-of-the-server]__42156b42._.js")
-R.m(80558)
-module.exports=R.m(80558).exports
+R.c("server/chunks/ssr/[root-of-the-server]__f80dff44._.js")
+R.m("[project]/node_modules/.pnpm/next@15.5.5_@babel+core@7.2_aec852d797e571d83e438071d3e163a7/node_modules/next/app.js [ssr] (ecmascript)")
+module.exports=R.m("[project]/node_modules/.pnpm/next@15.5.5_@babel+core@7.2_aec852d797e571d83e438071d3e163a7/node_modules/next/app.js [ssr] (ecmascript)").exports
