a624a1514dbbfdfcd7d1ed98c7f72bf1db3da26e
abdulvohid31 <abdulvohidcode@gmail.com>
2025-10-27 11:26:11 +0500

    icons added

--- src/features/change-language/ui/index.tsx
+++ src/features/change-language/ui/index.tsx
@@ -1,39 +1,46 @@
-'use client';
+"use client";
 
-import { getLocalItem, setLocalItem } from '@/shared/utils/storage';
-
-import { LANGUAGES } from '@/shared/constants';
-import { Select } from 'antd';
-import { usePathname } from '@/i18n/routing';
-import { useRouter } from 'next/navigation';
+import { getLocalItem, setLocalItem } from "@/shared/utils/storage";
+import { LANGUAGES } from "@/shared/constants";
+import { Select } from "antd";
+import { usePathname } from "@/i18n/routing";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import "../style/style.css";
 
 export default function LanguageSwitcher() {
   const router = useRouter();
   const pathname = usePathname();
 
-  const pathParts = pathname.split('/').filter(Boolean); // removes empty strings
+  const pathParts = pathname.split("/").filter(Boolean);
+
   const handleChange = (value: string) => {
-    setLocalItem('lang', value);
-    let newPath = '';
-    if (pathParts.length > 0) {
-      newPath += `/${value}${pathname}`;
-    } else newPath += '/' + value;
+    setLocalItem("lang", value);
+    let newPath = "";
+    if (pathParts.length > 0) newPath += `/${value}${pathname}`;
+    else newPath += "/" + value;
     router.push(newPath);
   };
 
   return (
-    <div className="flex items-center gap-2">
-      <select 
-        value={getLocalItem('lang') || 'uz'}
-        onChange={(e) => handleChange(e.target.value)}
-        className="py-3 px-3 rounded-full global-select-border"
-        >
-          {LANGUAGES.map((lang: { code: string; label: string }) => (
-            <option key={lang.code} value={lang.code}>
-              {lang.label}
-            </option>
-          ))}
-        </select>
-    </div>
+    <Select
+      value={getLocalItem("lang") || "uz"}
+      onChange={handleChange}
+      className="custom-lang-select w-[150px] !min-h-[50px]"
+      dropdownStyle={{
+        padding: "10px 6px ",
+        borderRadius: "10px",
+      }}
+      popupClassName="custom-lang-dropdown"
+    >
+      {LANGUAGES.map((lang) => (
+        <Select.Option key={lang.code} value={lang.code}>
+          <div className="flex items-center gap-2">
+            <Image src={lang.icon} alt={lang.label} width={20} height={20} />
+            <span className="text-[18px] font-medium">{lang.label}</span>
+          </div>
+        </Select.Option>
+      ))}
+    </Select>
   );
 }
--- src/shared/constants/header.ts
+++ src/shared/constants/header.ts
@@ -1,7 +1,12 @@
+import TurkeyFlag from "@/assets/images/landing-images/turkey.png";
+import RussiaFlag from "@/assets/images/landing-images/russia.png";
+import ChinaFlag from "@/assets/images/landing-images/china.png";
+import uzFlag from "@/assets/images/landing-images/uzb.png";
+import ukFlag from "@/assets/images/landing-images/united-kingdom.png";
 export const LANGUAGES = [
-  { code: "en", label: "English" },
-  { code: "uz", label: "O‘zbekcha" },
-  { code: "ru", label: "Русский" },
-  { code: "zh", label: "中文" },
-  { code: "tr", label: "Türkçe" },
+  { code: "en", label: "English", icon: ukFlag },
+  { code: "uz", label: "O‘zbekcha", icon: uzFlag },
+  { code: "ru", label: "Русский", icon: RussiaFlag },
+  { code: "zh", label: "中文", icon: ChinaFlag },
+  { code: "tr", label: "Türkçe", icon: TurkeyFlag },
 ];
