5277f17ae0f0ae4683851e1342e05220492f23ad
Alisher0903 <alishersodiqov09@gmail.com>
2025-10-27 14:21:40 +0500

    updates

--- src/features/logout/logout-modal.tsx
+++ src/features/logout/logout-modal.tsx
@@ -1,46 +1,46 @@
-import { useRouter } from "@/i18n/routing";
-import { authService } from "@/services/auth/auth.service";
-import { ButtonPrimary } from "@/shared/components/dump/atoms";
-import { useGlobalStore } from "@/shared/store/globalStore";
-import { Button, Modal } from "antd";
-import { useTranslations } from "next-intl";
+import { Button, Modal } from 'antd';
+
+import { authService } from '@/services/auth/auth.service';
+import { useGlobalStore } from '@/shared/store/globalStore';
+import { useRouter } from '@/i18n/routing';
+import { useTranslations } from 'next-intl';
 
 const LogoutModal = () => {
   const { isLogout, setIslogout } = useGlobalStore();
   const router = useRouter();
-  const t = useTranslations("logoutModal");
+  const t = useTranslations('logoutModal');
   return (
     <Modal
-      title={t("title")}
-      closable={{ "aria-label": "Custom Close Button" }}
+      title={t('title')}
+      closable={{ 'aria-label': 'Custom Close Button' }}
       open={isLogout}
       okType="danger"
       centered
       onCancel={() => setIslogout(false)}
       footer={
-        <div className="flex gap-2">
-          <button
-            type="button"
-            className="bg-secondary-blue-color px-6 py-0 !text-white rounded-xl"
-            onClick={async () => {
-              await authService.clearStorage();
-              router.push("/");
-              setIslogout(false);
-            }}
-          >
-            {t("okText")}
-          </button>
+        <div className="flex justify-end gap-2">
           <Button
             className="!px-6 !py-4 !rounded-xl"
             type="text"
             onClick={() => setIslogout(false)}
           >
-            {t("cancelText")}
+            {t('cancelText')}
           </Button>
+          <button
+            type="button"
+            className="bg-secondary-blue-color cursor-pointer px-6 py-0 !text-white rounded-xl"
+            onClick={async () => {
+              await authService.clearStorage();
+              router.push('/');
+              setIslogout(false);
+            }}
+          >
+            {t('okText')}
+          </button>
         </div>
       }
     >
-      <p>{t("confirmationMessage")}</p>
+      <p>{t('confirmationMessage')}</p>
     </Modal>
   );
 };
