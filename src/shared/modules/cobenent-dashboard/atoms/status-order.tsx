b6c26056802c0d296831c1e44ac95ac4f0126e46
abdulazizsaidovcode <abdulazizsaidovcode@gmail.com>
2025-11-08 19:28:08 +0500

    add: card status qoydim

--- src/shared/modules/cobenent-dashboard/atoms/status-order.tsx
+++ src/shared/modules/cobenent-dashboard/atoms/status-order.tsx
@@ -16,9 +16,16 @@ export default function StatusOrder({ status }: { status: string }) {
     }
   }
 
+  const statusStyle: Record<string, string> = {
+    Pending_Approval: "bg-gray-400 text-white border-gray-500",
+    Closed: "bg-green-500 text-white",
+    Pending_Billing: "bg-yellow-500 text-white",
+    Default: "bg-gray-400 text-white border-gray-500",
+  };
+
   return (
-    <div className=" bg-green-600 w-max text-white font-semibold  py-1 px-2 flex items-center justify-center text-sm rounded-xl">
-      <h1 className="text-md w-fit !m-0 !p-0">{t(formatStatus(status))}</h1>
+    <div className={`w-max font-semibold  py-1 px-2 flex items-center justify-center text-sm rounded-xl ${statusStyle[formatStatus(status)]}`}>
+      <h1 className={`text-md w-fit !m-0 !p-0`}>{t(formatStatus(status))}</h1>
     </div>
   );
 }
