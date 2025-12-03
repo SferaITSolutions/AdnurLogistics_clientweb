5a1de8e9f48aad20e290475cb69fc8cf14d23f20
abdulazizsaidovcode <abdulazizsaidovcode@gmail.com>
2025-12-03 14:26:48 +0500

    go go go

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin();
 const nextConfig: NextConfig = {
+  output: 'export',
   eslint: {
     ignoreDuringBuilds: true, // agar lint xatolari buildni buzsa
   },
